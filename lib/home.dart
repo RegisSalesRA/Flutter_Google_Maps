dc6fe8863f53734020ad18b3754bfb396a42d197
RegisSalesRA <regismsales@gmail.com>
2021-07-03 12:46:22 -0300

    GoogleMap implementated

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _controller = Completer();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          //
+          initialCameraPosition:
+              CameraPosition(target: LatLng(-23.562436, -46.655005), zoom: 16),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+}
