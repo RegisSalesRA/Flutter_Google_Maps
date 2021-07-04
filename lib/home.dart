3cab3ff33eca07c66b1c402a7f0c1cf8556cbc39
RegisSalesRA <regismsales@gmail.com>
2021-07-03 22:11:36 -0300

    Movimentando Camera do mapa

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,13 +10,41 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController _googleMapController;
+  _onMapController(GoogleMapController googleMapController) {
+    _controller.complete(googleMapController);
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+          target: LatLng(-23.562436, -46.655005),
+          zoom: 16,
+          //Angulação   45 graus = tilt: 45
+          tilt: 45,
+          // Bearing é a rotação
+          bearing: 270),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Mapas e Geolocalização"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.done),
+        onPressed: _movimentarCamera,
+      ),
       body: Container(
+        //Criaçao do mapa parte desse widget
         child: GoogleMap(
           mapType: MapType.normal,
-          //
+
+          //Posição inicial da Camera
           initialCameraPosition:
               CameraPosition(target: LatLng(-23.562436, -46.655005), zoom: 16),
           onMapCreated: (GoogleMapController controller) {
