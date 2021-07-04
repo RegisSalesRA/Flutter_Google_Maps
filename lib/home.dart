09ea557817b54b96d8ace09a8d8e273c5ea1ef45
RegisSalesRA <regismsales@gmail.com>
2021-07-03 23:20:15 -0300

    Adicionando marcadores

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,8 +9,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
+
+  // ignore: unused_field
   GoogleMapController _googleMapController;
-  _onMapController(GoogleMapController googleMapController) {
+  Set<Marker> _marcadores = {};
+  _onMapCreated(GoogleMapController googleMapController) {
     _controller.complete(googleMapController);
   }
 
@@ -19,13 +21,41 @@ class _HomeState extends State<Home> {
     GoogleMapController googleMapController = await _controller.future;
     googleMapController.animateCamera(CameraUpdate.newCameraPosition(
       CameraPosition(
-          target: LatLng(-23.562436, -46.655005),
+          target: LatLng(-4.831256, -40.321122),
           zoom: 16,
           //Angulação   45 graus = tilt: 45
           tilt: 45,
           // Bearing é a rotação
           bearing: 270),
     ));
+  }
+
+  _carregarMarcadores() {
+    Set<Marker> marcadoresLocal = {};
+    Marker marcadorShopping = Marker(
+        markerId: MarkerId("tamboril-casa"),
+        position: LatLng(-4.831256, -40.321122),
+        infoWindow: InfoWindow(title: "Casa do lunard"),
+        // Icone padrao BitmapDescriptor.defaultMarker ( Icone vermelho marcando o ponto no mapa)
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
+        //rotation: 45
+        onTap: () {
+          print("Clicou casa do lunard");
+        });
+
+    marcadoresLocal.add(marcadorShopping);
+    setState(() {
+      _marcadores = marcadoresLocal;
+    });
+  }
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    _carregarMarcadores();
   }
 
   @override
@@ -42,14 +72,18 @@ class _HomeState extends State<Home> {
       body: Container(
         //Criaçao do mapa parte desse widget
         child: GoogleMap(
-          mapType: MapType.normal,
+          mapType:
+              //mapType: MapType.hybrid,
+              //mapType: MapType.none,
+              //mapType: MapType.satellite,
+              //mapType: MapType.terrain,
+              MapType.normal,
 
           //Posição inicial da Camera
           initialCameraPosition:
-              CameraPosition(target: LatLng(-23.562436, -46.655005), zoom: 16),
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
+              CameraPosition(target: LatLng(-4.831256, -40.321122), zoom: 16),
+          onMapCreated: _onMapCreated,
+          markers: _marcadores,
         ),
       ),
     );
